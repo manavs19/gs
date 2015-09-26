7cd855c8de98e61d7b4cc979df3ec2ba46b6a929
manavs19 <manavcool.sethi@gmail.com>
2015-09-27 00:54:49 +0530

    yoyo 25.1467%

--- prob2.py
+++ prob2.py
@@ -21,12 +21,21 @@ data_map = {}
 classes = []
 classes_map = {}
 
+MEANDIFF=0
+
 with open(trainingFileName, 'rb') as csvfile:
 	reader = csv.reader(csvfile)
 	reader.next()
 	for line in reader:
 		row = []
 		numCols = len(line)
+		startTime=0
+		endTime=0
+
+		if (not line[8] or not line[12]):
+			continue
+		if getTimestamp(line[8]) > getTimestamp(line[12]):
+			continue
 		for i in range(1, numCols-1):
 			if not line[i]:#empty
 				row.append(float('nan'))
@@ -36,14 +45,19 @@ with open(trainingFileName, 'rb') as csvfile:
 				row.append(int(line[i]))
 				continue
 
-			if i==8 or i==12:#date
-				row.append(getTimestamp(line[i]))
+			if i==8:
+				startTime = getTimestamp(line[i])
+				continue
+
+			if i==12:
+				endTime = getTimestamp(line[i])
 				continue
 
 			if str(line[i]) not in data_map:
 				data_map[str(line[i])] = len(data_map)
 			row.append(data_map[str(line[i])])
-		
+		MEANDIFF=MEANDIFF + endTime - startTime
+		row.append(endTime - startTime)
 		data.append(row)
 
 		currClass = str(line[numCols-1])
@@ -51,6 +65,7 @@ with open(trainingFileName, 'rb') as csvfile:
 			classes_map[currClass] = len(classes_map)
 		classes.append(classes_map[currClass])
 
+MEANDIFF = float(MEANDIFF) / len(data)
 imp = Imputer()
 imp = imp.fit(data)
 data = imp.transform(data)
@@ -86,6 +101,19 @@ with open(testFileName, 'rb') as csvfile:
 		isins.append(str(line[0]))
 		row = []
 		numCols = len(line)
+		startTime=0
+		endTime=0
+		if (not line[8] or not line[12]):
+			line[8] = 1
+			line[12] = 1+ MEANDIFF
+		else:
+			line[8] = getTimestamp(line[8])
+			line[12] = getTimestamp(line[12])
+
+		if line[8] > line[12]:
+			line[8] = 1
+			line[12] = 1+MEANDIFF
+
 		for i in range(1, numCols):
 			if not line[i]:#empty
 				row.append(float('nan'))
@@ -95,15 +123,22 @@ with open(testFileName, 'rb') as csvfile:
 				row.append(int(line[i]))
 				continue
 
-			if i==8 or i==12:#date
-				row.append(getTimestamp(line[i]))
+			if i==8:
+				startTime = line[8]
+				continue
+
+			if i==12:
+				endTime = line[12]
 				continue
 
 			if str(line[i]) not in data_map:
 				data_map[str(line[i])] = len(data_map)
 			row.append(data_map[str(line[i])])
-		
+		row.append(endTime-startTime)
 		test_data.append(row)
+
+# for r in test_data:
+# 	print len(r)
 
 imp = Imputer()
 imp = imp.fit(test_data)
